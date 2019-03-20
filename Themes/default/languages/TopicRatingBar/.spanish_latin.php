2a1615608a6d66e06872e668378103c6e0c2daeb
Bomber Code <contacto@bombercode.net>
2019-03-20 09:45:45 -0300

    Translation to Latin Spanish!

---
+++ Themes/default/languages/TopicRatingBar/.spanish_latin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * .spanish_latin-utf8.php
+ *
+ * @package Topic Rating Bar
+ * @author Bugo https://dragomano.ru/mods/topic-rating-bar
+ * Spanish translation https://www.bombercode.net Copyright 2014-2019
+ */
+$txt['tr_title']        = 'Barra de calificaci&oacute;n de temas';
+$txt['tr_rating']       = 'Clasificaci&oacute;n del tema:';
+$txt['tr_top_stat']     = 'Las estad&iacute;sticas de popularidad comunes';
+$txt['tr_top_topics']   = 'Los temas más populares';
+$txt['tr_top_desc']     = 'Aqu&iacute; puede ver los temas más populares (por calificaci&oacute;n y n&uacute;mero de votos). Tienes la capacidad de ordenar esta tabla por cualquier columna.';
+$txt['tr_best_topic']   = 'El mejor tema';
+$txt['tr_other_topics'] = 'Otros temas populares';
+$txt['tr_top_empty']    = 'No hay clasificaci&oacute;nes a&uacute;n.';
+$txt['tr_rate_pl']      = 'Califica este tema: ';
+$txt['tr_currently']    = 'Actualmente: ';
+$txt['tr_average']      = 'Puntuaci&oacute;n media';
+$txt['tr_rates']        = 'Horrible|Malo|M&iacute;nimo|Bueno|Excelente';
+$txt['tr_rates_10']     = 'Peor en ninguna parte|Estupendo|Malo|Pasable|Promedio|No está mal|Bien|Muy bien|Perfecto|Excelente';
+$txt['tr_votes']        = 'Total de votos: ';
+$txt['tr_desc']         = 'Esta modificaci&oacute;n le ayuda a crear un sistema de clasificaci&oacute;n de temas en su foro.';
+$txt['tr_rate_system']     = 'Sistema de calificaci&oacute;n';
+$txt['tr_system_array']    = array('5 estrellas', '10 estrellas');
+$txt['tr_mini_rating']     = 'Mostrar mini-rating en el &iacute;ndice de mensajes.';
+$txt['tr_show_best_topic'] = 'Mostrar el mejor tema en el &iacute;ndice del foro';
+$txt['tr_count_topics']    = 'N&uacute;mero de temas en <a href="%1$s?action=rating" target="_blank">la página de estad&iacute;sticas comunes</a>';
+$txt['tr_ignore_boards']   = 'Foros ignorados';
+$txt['tr_regular_members'] = 'Miembros regulares';
+$txt['groups_rate_topics']         = '&iquest;Qui&eacute;n puede cambiar las calificaciones de los temas?';
+$txt['permissionname_rate_topics'] = 'Clasificaci&oacute;n de los temas';
+$txt['permissionhelp_rate_topics'] = 'Los usuarios pueden calificar cualquier tema (no propio) en los foros permitidos.';
+?>
